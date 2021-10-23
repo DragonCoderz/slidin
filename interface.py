2fa3cb9cbb508af35e52ebbc1b33e7dfad7f6a67
ErisedKT <sanyaverma0303@gmail.com>
2021-10-23 13:43:35 -0400

    Add interfacing functions

---
+++ interface.py
@@ -0,0 +1,21 @@
+from collection_manager import *
+
+id_to_username = {}
+bucket = 'testimages-h'
+collection_id='collectionhackGT2021testing'
+
+def add_profile(username, pfp):
+    face = add_faces_to_collection(bucket,pfp,collection_id,1)
+    id_to_username[face[0]['Face']['FaceId']] = username
+
+def upload_picture(photo):
+    detected_faces=add_faces_to_collection(bucket,photo,collection_id,5)
+    for face in detected_faces:
+        face_id = face['Face']['FaceId']
+        matched_faces=search_faces_in_collection(face_id, collection_id)
+        for match in matched_faces:
+            match = match['Face']['FaceId']
+            if match in id_to_username:
+                id_to_username[face_id] = id_to_username[match]
+                break
+
