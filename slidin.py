5058645671e35ef224b86ad279157c5f7672fecb
Jesse Dill <jessedill123@gmail.com>
2021-10-23 13:44:45 -0400

    Created Basic framework of website

--- slidin.py
+++ slidin.py
@@ -1,9 +1,23 @@
-# save this as app.py
-from flask import Flask, escape, request
-
+from logging import debug
+from os import truncate
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    name = request.args.get("name", "World")
-    return f'Hello, {escape(name)}!'    
+posts = [
+    {
+        'date': 'this is a date',
+        'title': 'Title: Blog Post 1',
+        'user': 'I am this person',
+        'content': 'content stuff'
+    }
+]
+
+@app.route("/")
+@app.route("/createProfile")
+def createProfile():
+    return render_template('createProfile.html', posts=posts)
+@app.route("/search")
+def search():
+    return render_template('search.html')
+if __name__ == '__main__':
+    app.run(debug=True)
