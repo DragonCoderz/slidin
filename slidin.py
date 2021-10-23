23e92596dd005b69317602e79c999d567d18bb26
Jesse Dill <jessedill123@gmail.com>
2021-10-23 17:55:59 -0400

    for UserImagePost, added users for all people in image

--- slidin.py
+++ slidin.py
@@ -32,8 +32,11 @@ class User(db.Model):
 
 class UserImagePost(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-
+    
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    #one to many relationship, one image to many users in image?
+    users = db.relationship('User', backref='image', lazy=True)
 
     #email = db.Column(db.String(120), unique=True, nullable=False) 
     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
