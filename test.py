f33ae8f8425e69afaf93a09a9fc349454d4c1446
ErisedKT <sanyaverma0303@gmail.com>
2021-10-23 14:57:23 -0400

    Add testing code

---
+++ test.py
@@ -0,0 +1,9 @@
+from interface import *
+
+add_profile('marco', 'p2.jpeg')
+add_profile('jessie', 'rightP.PNG')
+add_profile('saoirse', 'p3.jpeg')
+add_profile('susan', 'p4.jpg')
+
+upload_picture('wrongG.PNG')
+
